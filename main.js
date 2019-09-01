943c3cbe000215c8e507240f59d1cdf30369a3ea
Chaymee <fr.golgo@gmail.com>
2019-09-01 17:59:43 -0500

    Added new file bar options and started support for javascript

--- main.js
+++ main.js
@@ -10,27 +10,38 @@ let addWindow
 // listen for app to be ready
 app.on('ready', function(){
     // create a new window
-    mainWindow = new BrowserWindow({});
+    mainWindow = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
     // Load html into window
     mainWindow.loadURL(url.format({
         pathname: path.join(__dirname, 'mainWindow.html'),
         protocol: 'file:',
         slashes: true
     }));
-
-    // Build mennu from template
+    // Quit app when Main window closes
+    mainWindow.on('closed', function(){
+        app.quit();
+    });
+    // Build menu from template
     const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
     // Insert menu
     Menu.setApplicationMenu(mainMenu);
-});
+})
+
 
 // Handle new certificate window
 function createNewWindow(){
     // create a new window
     newWindow = new BrowserWindow({
-        width: 400,
-        height: 600,
-        title: 'Enter parameters for new certificate'
+        width: 450,
+        height: 450,
+        title: 'Enter parameters for new certificate',
+        webPreferences: {
+            nodeIntegration: true
+        }
     });
     // Load html into window
     newWindow.loadURL(url.format({
@@ -38,13 +49,19 @@ function createNewWindow(){
         protocol: 'file:',
         slashes: true
     }));
-}
+    // Clear memory from closed window
+    /*
+    createNewWindow.on('closed', function(){
+        createNewWindow = null;
+    });
+    */
+};
 
 // Create Menu template
 const mainMenuTemplate = [
     {
         label: 'File',
-        submenu: [
+        submenu:[
             {
             label: 'New Certificate',
             accelerator: process.platform == 'darwin' ? 'Command+N' :
@@ -63,4 +80,30 @@ const mainMenuTemplate = [
         }
     ]
     }
-]
+];
+
+// If mac, add empty object to menu
+/*
+if(process.platform == 'darwin') {
+    mainMenuTemplate.unshift({});
+}
+*/
+// Add developer tools item if not in production
+if(process.env.NODE_ENV !== 'production'){
+    mainMenuTemplate.push({
+        label: 'Developer Tools',
+        submenu:[
+            {
+                label: 'Toggle DevTools',
+                accelerator: process.platform == 'darwin' ? 'Command+I' : 
+                'Ctrl+I',
+                click(item, focusedWindow){
+                    focusedWindow.toggleDevTools();
+                }
+            },
+            {
+                role: 'reload'
+            }
+        ]
+    })
+}
